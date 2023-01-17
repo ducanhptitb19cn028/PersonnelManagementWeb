e402408fe2e0e5cbb16fd6806efdcc84441509f8
phucanhprono1 <90205324+phucanhprono1@users.noreply.github.com>
2023-01-17 15:36:38 +0700

    lkj

--- src/main/java/dajava/personnelmanagementweb/service/UserService.java
+++ src/main/java/dajava/personnelmanagementweb/service/UserService.java
@@ -1,4 +1,8 @@
 package dajava.personnelmanagementweb.service;
 
+import dajava.personnelmanagementweb.dto.UserRegistrationDTO;
+import dajava.personnelmanagementweb.model.User;
+
 public interface UserService {
+    User save(UserRegistrationDTO registrationDto);
 }
